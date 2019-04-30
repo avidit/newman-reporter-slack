ba93c0e883d2ee418d1b9d727ca75ffe6b5d6eac
kcaabishkar <aabishkar@gmail.com>
2019-04-30 13:21:29 -0400

    Add reporterOptions
    - Add ability to pass webhookUrl, channel, title and header as reporterOptions or environment variables

--- README.md
+++ README.md
@@ -2,17 +2,31 @@
 
 A [newman](https://github.com/postmanlabs/newman) reporter for [slack](https://slack.com/)
 
+newman run
+| collection - environment | total | failed  |
+|--------------------------|-------|---------|
+| iterations               | 1     | 0       |
+| requests                 | 1     | 0       |
+| testScripts              | 1     | 0       |
+| prerequestScripts        | 1     | 0       |
+| assertions               | 1     | 0       |
+| ------------------       | ----- | ------- |
+| total run duration       | 100ms |         |
+
 ## Installation
-    npm install newman-reporter-slack.git
+    npm install newman-reporter-slack
 
 ## Usage
 
-### Set the environment variables.
+### Set the reporter options
+
+Reporter option can be set as environment variables or from reporter options arguments
 ```
-SLACK_WEBHOOK_URL='https://hooks.slack.com/services/xxx/yyy/zzzzzzzzzzzz'
-SLACK_CHANNEL='#slack-channel'
-HEADER='newman run'
+export SLACK_WEBHOOK_URL='https://hooks.slack.com/services/xxx/yyy/zzzzzzzzzzzz'
+export SLACK_CHANNEL='#slack-channel'
+export TITLE='newman run title'
+export HEADER='newman run header'
 ```
 
 ### Run newman test with the reporter option `-r slack`
-    newman run my-collection.postman_collection.json -r cli,slack
+    newman run my-collection.postman_collection.json -r cli,slack --reporter-slack-channel '#slack-channel' --reporter-slack-webhook-url 'https://hooks.slack.com/services/xxx/yyy/zzzzzzzzzzzz'
--- index.js
+++ index.js
@@ -4,18 +4,36 @@ let {
 let markdowntable = require('markdown-table');
 let prettyms = require('pretty-ms');
 
-const webhook_url = process.env.SLACK_WEBHOOK_URL || '';
-const channel = process.env.SLACK_CHANNEL || '';
-const header = process.env.HEADER || 'newman run';
-
 
 class SlackReporter {
-    constructor(emitter, reporterOptions, options) {
+    constructor(emitter, reporterOptions) {
         const backticks = '```';
+        const webhookUrl = process.env.SLACK_WEBHOOK_URL || reporterOptions.webhookUrl;
+        const channel = process.env.SLACK_CHANNEL || reporterOptions.channel;
+        let title = process.env.TITLE || reporterOptions.title;
+        let header = process.env.HEADER || reporterOptions.header || '';
+
+        if (!webhookUrl) {
+            console.log('please provide slack webhook url');
+            return;
+        }
+        if (!channel) {
+            console.log('please provide slack channel');
+            return;
+        }
 
         emitter.on('done', (err, summary) => {
+            if (err) {
+                return;
+            }
             let run = summary.run;
             let data = []
+            if (!title) {
+                title = summary.collection.name;
+                if (summary.environment.name) {
+                    title += ' - ' + summary.environment.name
+                }
+            }
             let headers = [header, 'total', 'failed'];
             let arr = ['iterations', 'requests', 'testScripts', 'prerequestScripts', 'assertions'];
 
@@ -29,17 +47,16 @@ class SlackReporter {
             data.push(['total run duration', duration]);
 
             let table = markdowntable(data);
-            let text = `${backticks}${table}${backticks}`
-
+            let text = `${title}\n${backticks}${table}${backticks}`
             let msg = {
                 channel: channel,
                 text: text
             }
 
-            const webhook = new IncomingWebhook(webhook_url);
+            const webhook = new IncomingWebhook(webhookUrl);
             webhook.send(msg, (error, response) => {
                 if (error) {
-                    return console.log(error.message);
+                    return console.error(error.message);
                 }
                 console.log(response);
             });
