4671d49383963f0f80d578138613f082c635a2ca
Max Sutton <maxsutton@Max-QA-iMac-2.local>
2019-10-24 10:39:23 -0700

    added functionality showing a list of failed asserts and the associated error by adding --verbose as a cli arg

--- index.js
+++ index.js
@@ -22,11 +22,14 @@ class SlackReporter {
             return;
         }
 
+        let verbose = process.argv.includes("--verbose")
+
         emitter.on('done', (err, summary) => {
             if (err) {
                 return;
             }
             let run = summary.run;
+            let totalFailures = summary.run.failures
             let data = []
             if (!title) {
                 title = summary.collection.name;
@@ -35,6 +38,7 @@ class SlackReporter {
                 }
             }
             let headers = [header, 'total', 'failed'];
+            let headersFailures = ['request failures', 'assertion', 'error']
             let arr = ['iterations', 'requests', 'testScripts', 'prerequestScripts', 'assertions'];
 
             data.push(headers);
@@ -42,9 +46,27 @@ class SlackReporter {
                 data.push([element, run.stats[element].total, run.stats[element].failed]);
             });
 
+
+
+
             let duration = prettyms(run.timings.completed - run.timings.started);
-            data.push(['------------------', '-----', '-------']);
+            data.push(['------------------', '---------------------------------', '------------------------------']);
             data.push(['total run duration', duration]);
+            data.push(['------------------', '---------------------------------', '------------------------------']);
+            if (verbose) {
+                data.push(headersFailures)
+                data.push(['------------------', '---------------------------------', '------------------------------']);
+
+                function pushErrors(index) {
+                    let failures = summary.run.failures[index];
+                    let request = failures.source.name;
+                    let failuresTest = failures.error.test
+                    let failuresMessage = failures.error.message
+                    data.push([request, failuresTest, failuresMessage])
+                }
+                totalFailures.forEach((failure, index) => pushErrors(index))
+            }
+
 
             let table = markdowntable(data);
             let text = `${title}\n${backticks}${table}${backticks}`
